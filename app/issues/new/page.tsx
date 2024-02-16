ed1544cc2039e6236f63f5ad3aefa8df91b0e4f2
topdev101 <ritachi0502@gmail.com>
2024-02-16 06:08:56 -0500

    Refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -12,6 +12,7 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import { assert } from "console";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -28,6 +29,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+      try {
+        setSubmitting(true);
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      } catch (error) {
+        setSubmitting(false);
+        setError("An unexpected error occurred.");
+      }
+  })
+
   return (
     <div className="max-w-x1">
       {error && (
@@ -37,16 +49,7 @@ const NewIssuePage = () => {
       )}
       <form
         className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An unexpected error occurred.");
-          }
-        })}
+        onSubmit={onSubmit}
       >
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />

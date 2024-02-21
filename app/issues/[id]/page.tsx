520e804ff05692b8d8c354d69fbbace7f48475f4
topdev101 <ritachi0502@gmail.com>
2024-02-21 14:31:29 -0500

    Improved the style

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
-import React from "react";
+import ReactMarkdown from 'react-markdown'
 
 interface Props {
   params: { id: string };
@@ -22,8 +22,8 @@ const IssueDetailPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.createdAt.toDateString()}</p>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   );

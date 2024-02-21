3d7d46c95b127daf5ea1240f169a4f6a63c35ad7
topdev101 <ritachi0502@gmail.com>
2024-02-21 14:05:18 -0500

    Style the issue detail page

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,7 +1,7 @@
 import { title } from 'process'
 import {z } from 'zod'
 
-export const createIssueSchema = z.object({
+export const issueSchema = z.object({
     title: z.string().min(1, 'Title is required.').max(255),
     description: z.string().min(1, 'Description is required')
 })
